5aaa2aff9e0fc3649ebf42fcb95387e6e87aa2b3
Longuet2 <tomlonguet77@gmail.com>
2025-11-20 12:09:31 +0100

    Implement player name prompts on Enter key
    Added functionality for player name input on game start.

--- static/js/script_acceuil.js
+++ static/js/script_acceuil.js
@@ -1,5 +1,22 @@
-document.addEventListener("keydown", function(e) {
+var joueur1;
+var joueur2;
+
+document.addEventListener("keydown", function(e) { //lance le jeu et demande le nom des joueurs quand on appuie sur entrer
     if (e.key === "Enter") {
+        identite()
         window.location.href = "/jeu";
     }
 });
+
+function identite() //permet aux joueurs d'entrer leurs noms
+	{joueur1=prompt('Saisissez le nom du joueur 1.','Joueur 1');
+	 if(joueur1 == '')
+	   {
+	    joueur1 = 'Joueur 1';
+	   }
+     joueur2 = prompt("Entrez un nom pour le joueur 2 :","Joueur 2");
+     if(joueur2 == '')
+        {
+         joueur2 = 'Joueur 2';
+        }
+	}
